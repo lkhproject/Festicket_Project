ce770162cb2827f553dc1a0b56d22a47751d56a2
Minji287 <dpa357@gmail.com>
2023-06-12 17:45:56 +0900

    행사등록 sql, 값 넘어가는지 확인필요

--- src/main/java/com/festicket/controller/AdminController.java
+++ src/main/java/com/festicket/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.festicket.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -62,67 +65,54 @@ public class AdminController {
 	}
 	
 	@RequestMapping(value = "/adminEventAdd")
-   public String adminList(Model model, HttpSession session, HttpServletRequest request) {
+	public String adminEventAdd(HttpSession session, Model model) {
+		
+		String sessionId = (String)session.getAttribute("sessionId");
+		
+		return "adminEventAdd";
+	}
+	
+	@RequestMapping(value = "/adminEventAddOk")
+   public String adminEventAddOk(Model model, HttpSession session, HttpServletRequest request) throws ParseException {
       
       String sessionId = (String)session.getAttribute("sessionId");
       
       IDao dao = sqlSession.getMapper(IDao.class);
       
+      String type = request.getParameter("type");
+       String gunName = request.getParameter("gunName");
+       String title = request.getParameter("title");
+       String place = request.getParameter("place");
+       String org_name = request.getParameter("org_name");
+       String use_trgt = request.getParameter("use_trgt");
+       String player = request.getParameter("player");
+       String program = request.getParameter("program");
+       String org_link = request.getParameter("org_link");
+       String main_img = request.getParameter("main_img");
+       String rgstDateStr = request.getParameter("rgstDate");
+       
+       Date rgstDate = null;
+	   SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	   rgstDate = dateFormat.parse(rgstDateStr);
+               
+       String start_dateStr = request.getParameter("start_date");
+	   Date start_date = null;
+	   start_date = dateFormat.parse(start_dateStr);
+               
+       String end_dateStr = request.getParameter("end_date");
+       Date end_date = null;
+	   end_date = dateFormat.parse(end_dateStr);
+               
+       String eventPrice = request.getParameter("eventPrice");
+       int ticketCount = Integer.parseInt(request.getParameter("ticketCount"));
+       
+       String eventDate = start_dateStr + "~" + end_dateStr;
       
-
-//      String type = request.getParameter("type");
-//       String gunName = request.getParameter("gunName");
-//       String title = request.getParameter("title");
-//       String place = request.getParameter("place");
-//       String org_name = request.getParameter("org_name");
-//       String use_trgt = request.getParameter("use_trgt");
-//       String player = request.getParameter("player");
-//       String program = request.getParameter("program");
-//       String org_link = request.getParameter("org_link");
-//       String main_img = request.getParameter("main_img");
-//       String rgstDateStr = request.getParameter("rgstDate");
-//       Date rgstDate = null;
-//       if (rgstDateStr != null && !rgstDateStr.isEmpty()) {
-//           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//           try {
-//               rgstDate = dateFormat.parse(rgstDateStr);
-//           } catch (ParseException e) {
-//               // 날짜 형식이 잘못된 경우 예외 처리
-//               e.printStackTrace();
-//           }
-//       }
-//       String start_dateStr = request.getParameter("start_date");
-//       Date start_date = null;
-//       if (start_dateStr != null && !start_dateStr.isEmpty()) {
-//           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//           try {
-//               start_date = dateFormat.parse(start_dateStr);
-//           } catch (ParseException e) {
-//               // 날짜 형식이 잘못된 경우 예외 처리
-//               e.printStackTrace();
-//           }
-//       }
-//       String end_dateStr = request.getParameter("end_date");
-//       Date end_date = null;
-//       if (end_dateStr != null && !end_dateStr.isEmpty()) {
-//           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//           try {
-//               end_date = dateFormat.parse(end_dateStr);
-//           } catch (ParseException e) {
-//               // 날짜 형식이 잘못된 경우 예외 처리
-//               e.printStackTrace();
-//           }
-//       }
-//       String eventPrice = request.getParameter("eventPrice");
-//       int ticketCount = Integer.parseInt(request.getParameter("ticketCount"));
-//       
-//       String eventDate = start_dateStr + "~" + end_dateStr;
-//      
-//      dao.eventAddDao(type, gunName, title, eventDate, place, org_name, use_trgt,
-//            player, program, org_link, main_img, rgstDate,
-//            start_date, end_date, eventPrice, ticketCount);
+      dao.eventAddDao(type, gunName, title, eventDate, place, org_name, use_trgt,
+            player, program, org_link, main_img, rgstDate,
+            start_date, end_date, eventPrice, ticketCount);
       
-      return "adminEventAdd";
+      return "adminEventAddOk";
    }
 	
 }
