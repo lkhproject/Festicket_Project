4e3301ea9ec3d0f9fee9899fdfac9817269be3bb
Mauve <Mauve@Mauve>
2023-06-14 11:23:13 +0900

    로그인하지 않았으면 접근불가 기능 추가

--- src/main/java/com/festicket/controller/ReviewController.java
+++ src/main/java/com/festicket/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.festicket.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.festicket.dao.IDao;
+import com.festicket.dto.Criteria;
+import com.festicket.dto.PageDto;
+import com.festicket.dto.ReserveDto;
 
 @Controller
 public class ReviewController {
@@ -19,20 +23,59 @@ public class ReviewController {
 	private SqlSession sqlSession;
 	
 	@RequestMapping(value = "/myPageReview")
-	public String myPageReview() {
+	public String myPageReview(HttpServletRequest request, Model model, HttpSession session, Criteria criteria) {
 		
-//		IDao dao = sqlSession.getMapper(IDao.class);
+		String sessionId = (String)session.getAttribute("sessionId");
 		
-//		int eventNum = Integer.parseInt(request.getParameter("selectedEvent"));
-//		
-//		model.addAttribute("event", dao.getEventDao(eventNum));
-//		model.addAttribute("reviewDtos", dao.getReviewListDao(eventNum));
+		int loginOk = 0;
 		
-		return "myPageReview";
+		// 세션 아이디가 없다면 접근불가
+		if(sessionId == null || sessionId.isEmpty()) {
+			request.setAttribute("loginOk", loginOk);
+		} else {
+			loginOk = 1;
+			
+			IDao dao = sqlSession.getMapper(IDao.class);
+			
+			// 페이징
+			int pageNum = 0;
+			
+			// 처음에는 request 객체에 넘어오는 값이 없기 떄문에 null 값이 옴
+			if(request.getParameter("pageNum") == null) {
+				pageNum = 1;
+				criteria.setPageNum(pageNum);
+			} else {
+				pageNum = Integer.parseInt(request.getParameter("pageNum"));
+				criteria.setPageNum(pageNum);
+			}
+			
+			int totalCount = dao.getReservationCountDao(sessionId);
+			
+			PageDto pageDto = new PageDto(criteria, totalCount);
+			
+			List<ReserveDto> revListDtos = dao.getReservationListDao(sessionId ,criteria.getCountList(), pageNum);
+			
+			request.setAttribute("loginOk", loginOk);
+			request.setAttribute("totalCount", totalCount);
+			model.addAttribute("pageMaker", pageDto);
+			model.addAttribute("revListDtos", revListDtos);
+			model.addAttribute("currPage", pageNum);
+			}
+			return "myPageReview";
 	}
 	
 	@RequestMapping(value = "/reviewWrite")
-	public String reviewWrite() {
+	public String reviewWrite(HttpServletRequest request, Model model, HttpSession session) {
+		String sessionId = (String)session.getAttribute("sessionId");
+		
+		int loginOk = 0;
+		
+		// 세션 아이디가 없다면 접근불가
+		if(sessionId == null || sessionId.isEmpty()) {
+			request.setAttribute("loginOk", loginOk);
+		} else {
+			loginOk = 1;
+		}
 		return "reviewWrite";
 	}
 	
