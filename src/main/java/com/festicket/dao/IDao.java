5d7b1439c5e5d8ee7d79fef5774c7f026699356a
천사랑 <@DESKTOP-70PDUQ0>
2023-06-14 09:00:58 +0900

    회원가입

--- src/main/java/com/festicket/dao/IDao.java
+++ src/main/java/com/festicket/dao/IDao.java
@@ -1,22 +1,10 @@
 package com.festicket.dao;
 
 import java.util.Date;
-import java.util.List;
 
-import com.festicket.dto.EventDto;
 import com.festicket.dto.MemberDto;
 
 public interface IDao {
-	
-	// 페스티벌
-	public List<EventDto> festivalListDao(int countList, int countPage); // 페스티벌 리스트
-	public int totalFestivalCountDao();
-	public List<EventDto> top5FestivalListDao(); // 페스티벌 탑5 리스트
-	
-	// 전시
-	public List<EventDto> exhibitionListDao(int countList, int countPage); // 전시 리스트
-	public int totalExhibitionCountDao();
-	public List<EventDto> top5ExhibitionListDao(); // 전시 탑5 리스트
 		
 	//회원관리	
 	public int joinDao(String userId, String userPassword, String userPhone, String email, String name, Date signupDate); //회원가입
