d022ea1b158c28f50182a2082522a51e60320718
Mauve <Mauve@Mauve>
2023-06-15 15:32:39 +0900

    주석 수정

--- src/main/java/com/festicket/dto/EventDto.java
+++ src/main/java/com/festicket/dto/EventDto.java
@@ -23,7 +23,7 @@ public class EventDto {
    private String program; // 프로그램소개
    private String org_link; // 홈페이지주소
    private String main_img; // 대표이미지
-   private String rgstDate; // 신청일
+   private String rgstDate; // 등록일
    private Date start_date; // 시작일
    private Date end_date; // 종료일
    private String eventPrice; // 이용요금
