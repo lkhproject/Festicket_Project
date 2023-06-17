e38bb46adbf000bea4f7499c10161c30c5f0dc27
Mauve <Mauve@Mauve>
2023-06-17 14:02:49 +0900

    행사 총 건수 찍히게

--- src/main/java/com/festicket/controller/ReservationController.java
+++ src/main/java/com/festicket/controller/ReservationController.java
@@ -3,6 +3,7 @@ package com.festicket.controller;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.festicket.dao.IDao;
+import com.festicket.dto.CSboardDto;
+import com.festicket.dto.ReviewDto;
 
 @Controller
 public class ReservationController {
@@ -27,6 +30,7 @@ public class ReservationController {
 		IDao dao = sqlSession.getMapper(IDao.class);
 		
 		model.addAttribute("event", dao.eventAllListDao());
+		model.addAttribute("totalCount", dao.totalEventCountDao());
 		
 		return "reservation/reservation";
 	}
@@ -42,7 +46,7 @@ public class ReservationController {
 		session.setAttribute("eventNum", eventNum);
 		
 		model.addAttribute("event", dao.getEventDao(eventNum));
-		model.addAttribute("reviewList", dao.reviewListDao(eventNum, 0, 0));
+		model.addAttribute("reviewList", dao.reviewListDao(eventNum));
 		model.addAttribute("QA_List", dao.getQAListDao(eventNum));
 		
 		return "reservation/rvView";
