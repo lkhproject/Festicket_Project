664c1d4431c595c6f6b80277385589ce8bd4720e
Minji287 <dpa357@gmail.com>
2023-06-19 17:10:13 +0900

    eventLikeClicked 함수 삭제

--- src/main/resources/static/js/rvView.js
+++ src/main/resources/static/js/rvView.js
@@ -1,31 +1,6 @@
 /**
  * 
  */
-
-  var isLiked = false; // 좋아요 상태를 나타내는 변수
-
-  // 버튼 클릭 이벤트 핸들러 함수
-function eventLikeClicked() {
-    var likeButton = document.getElementById("like_btn");
-    
-    if (isLiked) {
-      // 원래 색으로 돌아올 때
-      likeButton.classList.remove("bg-dark");
-      likeButton.classList.add("btn-light");
-      likeButton.classList.remove("text-white");
-      likeButton.classList.remove("text-bg-danger");
-      isLiked = false;
-    } else {
-      // 색 변경
-      likeButton.classList.add("bg-dark");
-      likeButton.classList.remove("btn-light");
-      likeButton.classList.add("text-white");
-      likeButton.classList.remove("text-bg-danger");
-      isLiked = true;
-    }
-  }
-
-
   
   var startIndex = 0;
   var endIndex = 5;
