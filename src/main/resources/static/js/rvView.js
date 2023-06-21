5d25e92151e55638f60ac4ef9b27932cd0003e8a
Mauve <Mauve@Mauve>
2023-06-21 14:14:22 +0900

    더보기 끝까지 안 나오는 문제 수정

--- src/main/resources/static/js/rvView.js
+++ src/main/resources/static/js/rvView.js
@@ -2,28 +2,41 @@
  * 
  */
   
-  var startIndex = 0;
-  var endIndex = 5;
+var startIndex = 0;
+var endIndex = 5;
 
-  function showMoreReviews() {
-    var reviewRows = document.getElementsByClassName("reviewRow");
-    for (var i = startIndex; i < endIndex; i++) {
-      if (reviewRows[i]) {
-        reviewRows[i].style.display = "table-row";
-      }
-    }
-    startIndex += 6;
-    endIndex += 6;
-    if (endIndex >= reviewRows.length) {
-      document.getElementById("showMoreRow").style.display = "none";
+function showMoreReviews() {
+  var reviewRows = document.getElementsByClassName("reviewRow");
+  var remainingReviews = reviewRows.length - startIndex;
+
+  var numToShow = Math.min(5, remainingReviews); // 최대 5개까지 표시하거나 남은 글 수만큼 표시합니다.
+
+  for (var i = startIndex; i < startIndex + numToShow; i++) {
+    if (reviewRows[i]) {
+      reviewRows[i].style.display = "table-row";
     }
   }
 
-  window.addEventListener("DOMContentLoaded", function() {
-    showMoreReviews();
-  });
- 
-  
-  
+  startIndex += numToShow;
 
-  
+  if (startIndex >= reviewRows.length) {
+    document.getElementById("showMoreRow").style.display = "none";
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  showMoreReviews();
+
+  var showMoreButton = document.getElementById("showMoreButton");
+  showMoreButton.addEventListener("click", function() {
+    showMoreReviews();
+
+    // 모든 리뷰 글이 표시되었으면 "더보기" 버튼 숨기기
+    var reviewRows = document.getElementsByClassName("reviewRow");
+    if (startIndex >= reviewRows.length) {
+      showMoreButton.style.display = "none";
+    }
+  });
+});
+
+ 
